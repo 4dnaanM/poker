85431aa9dfde0cde89bf6536801f2a3ed0706bc2
Adnaan Mansoor <4dnaanm@gmail.com>
2025-06-30 17:57:49 +0530

    noticed error when only one remaining, fix

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ mod player;
 use deck::Deck;
 use game::Game;
 fn main() {
-    let mut game = Game::new(5,100);
+    let mut game = Game::new(5,10000);
     // Deck::print_cards(Deck::new().deck);
-    for round in 0..3 {
+    for round in 0..10000 {
         game.play_round(round);
     }
 }
